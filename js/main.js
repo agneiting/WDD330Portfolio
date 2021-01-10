8d8d80a2d2cfa68665f78edd58b159126a056b5d
agneiting <59512232+agneiting@users.noreply.github.com>
2021-01-09 17:12:21 -0700

    index and main.js added

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Select ol element from page
+var toc = document.getElementById("toc");
+
+//Create Array
+let contents = [
+    ['Exercise 1', 'https://www.google.com'],
+    ['Exercise 2', 'https://www.facebook.com'],
+    ['Exercise 3', 'https://www.youtube.com']
+];
+
+//Read list of links from array and call function 
+for (let i = 0; i < contents.length; i++)
+    {
+        //Get label
+        var a = contents[i][0];
+        //Get URL
+        var b = contents[i][1];
+        //Call function
+        dynamicTOC(a, b);
+    }
+
+//Fuction that creates and adds content to table.
+function dynamicTOC(label, url) {
+    //Create li element
+    var li = document.createElement("li");
+    //Create a element
+    var a = document.createElement("a");
+    a.textContent = label;
+    a.setAttribute('href', url);
+    //Add a element to li element
+    li.appendChild(a);
+    //Add li to ol
+    document.getElementById("toc").appendChild(li);
+};
